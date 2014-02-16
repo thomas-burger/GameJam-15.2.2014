78e44033a261c0c504a7803385c96cb97778988d
Pavel Czempin <p.czempin@gmx.de>
2014-02-16 15:03:02 +0100

    initial fight prototype

--- Assets/Character/CharacterData.cs
+++ Assets/Character/CharacterData.cs
@@ -5,7 +5,8 @@ public class CharacterData : MonoBehaviour {
 	public static CharacterData singleton = null;
 
 	public int health = 10;
-	public int energy = 10;
+	public int energy = 100;
+	public int weaponlevel = 7;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +15,13 @@ public class CharacterData : MonoBehaviour {
 			singleton = this;
 		}
 
+	}
+
+	public void fight(int enemyLevel) {
+		int energyDelta = 2;//enemyLevel - weaponlevel;
+		int healthDelta = Mathf.Min (-energyDelta, 0); // never grow health
+		energy += energyDelta;
+		health += healthDelta;
 	}
 	
 	// Update is called once per frame
