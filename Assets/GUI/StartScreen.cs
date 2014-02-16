6d04e3c2a9261e3f613c29a9de4f7529f2a7001b
Nicolai Czempin <nicolai.czempin@gmail.com>
2014-02-16 23:20:21 +0100

    Added Quit button to main menu
    Added Quit button to main menu.
    
    fixes issue #2

--- Assets/GUI/StartScreen.cs
+++ Assets/GUI/StartScreen.cs
@@ -3,53 +3,55 @@ using System.Collections;
 
 public class StartScreen : GUIFrame
 {
-	public static StartScreen current = null;
-
-	private GUIStyle titleStyle = new GUIStyle();
-
-	string randomSeedString = "Insert random seed";
+		public static StartScreen current = null;
+		private GUIStyle titleStyle = new GUIStyle ();
+		string randomSeedString = "Insert random seed";
 	
-	// Use this for initialization
-	void Awake ()
-	{
-		if(current == null)
+		// Use this for initialization
+		void Awake ()
 		{
-			current = this;
+				if (current == null) {
+						current = this;
+				}
+				titleStyle.fontSize = 20;
+				titleStyle.alignment = TextAnchor.MiddleCenter;
+				titleStyle.normal.textColor = Color.white;
 		}
-		titleStyle.fontSize = 20;
-		titleStyle.alignment = TextAnchor.MiddleCenter;
-		titleStyle.normal.textColor = Color.white;
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	public override void Hide()
-	{
-		base.Hide ();
-		CharacterSelectionScreen.singleton.Hide ();
-	}
-	
-	public override void OnGUI()
-	{
-		if(!hidden)
+		// Update is called once per frame
+		void Update ()
 		{
-			base.OnGUI ();
-			float width = screenRect.width;
-			float height = screenRect.height;
-			//title
-			GUI.Label(new Rect(screenRect.x, screenRect.y, width, 0.2f*height), "GameJam(15.2.2014)", titleStyle);
-			//Input field for random seed
-			randomSeedString = GUI.TextField(new Rect(screenRect.center.x-100, screenRect.y+100, 200, 25), randomSeedString);
-			//character selection screen
-			CharacterSelectionScreen.singleton.Show(new Rect(screenRect.x, screenRect.y + 200, screenRect.width, screenRect.height-200));
-			//new game button
-			if(GUI.Button(new Rect(screenRect.center.x-50, screenRect.y+150, 100, 50), "Start game"))
-			{
-				Game.StartGame(randomSeedString.GetHashCode());
-			}
+				if (Input.GetKey ("escape")) {
+						Application.Quit ();
+				}
+
 		}
-	}
+
+		public override void Hide ()
+		{
+				base.Hide ();
+				CharacterSelectionScreen.singleton.Hide ();
+		}
+	
+		public override void OnGUI ()
+		{
+				if (!hidden) {
+						base.OnGUI ();
+						float width = screenRect.width;
+						float height = screenRect.height;
+						//title
+						GUI.Label (new Rect (screenRect.x, screenRect.y, width, 0.2f * height), "GameJam(15.2.2014)", titleStyle);
+						//Input field for random seed
+						randomSeedString = GUI.TextField (new Rect (screenRect.center.x - 100, screenRect.y + 100, 200, 25), randomSeedString);
+						//character selection screen
+						CharacterSelectionScreen.singleton.Show (new Rect (screenRect.x, screenRect.y + 200, screenRect.width, screenRect.height - 200));
+						//new game button
+						if (GUI.Button (new Rect (screenRect.center.x - 105, screenRect.y + 150, 100, 50), "Start game")) {
+								Game.StartGame (randomSeedString.GetHashCode ());
+						}
+						if (GUI.Button (new Rect (screenRect.center.x + 5, screenRect.y + 150, 100, 50), "Quit game")) {
+								Application.Quit ();
+						}
+				}
+		}
 }
